20eb8eff1f87b96ae824966e2600675b590b3282
cepoumian <cepoumian@gmail.com>
2025-12-16 12:34:23 -0600

    feat: create listing api models

---
+++ src/pods/listing/listing.api-model.ts
@@ -0,0 +1,35 @@
+export interface ListingListItemApiModel {
+  id: string;
+  name: string;
+  imageUrl: string | null;
+  price: number | null;
+  priceCurrency: string | null;
+  address: {
+    country: string;
+  };
+}
+
+export interface ReviewApiModel {
+  id: string;
+  date: string; // ISO string
+  reviewerName: string;
+  comments: string;
+}
+
+export interface ListingDetailApiModel {
+  id: string;
+  name: string;
+  description: string | null;
+  imageUrl: string | null;
+  amenities: string[];
+  address: {
+    country: string;
+    street: string | null;
+  };
+  reviews: ReviewApiModel[]; // últimas 5
+}
+
+export interface CreateReviewApiModel {
+  reviewerName: string;
+  comments: string;
+}
